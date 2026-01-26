d6f0170c9070058503ce68aeb500ae30d6f5ab77
Marzhmallow <m.zhylkaidarova@gmail.com>
2026-01-26 18:24:17 +0000

    dockerize fast api service

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -11,9 +11,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 if SECRET_KEY is None:
     print("WARNING: SECRET_KEY is NULL")
 
-DATABASE_URL=os.getenv("DATABASE_URL", "postgresql+psycopg2://app:app@192.168.20.43:5432/appdb")
+DATABASE_URL=os.getenv("DATABASE_URL", "postgresql+psycopg2://app:app@postgres:5432/appdb")
 
-KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:29092")
 
 S3_ENDPOINT = os.getenv("S3_ENDPOINT")
 S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
