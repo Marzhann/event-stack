9266c559cf4750b1347dd81aad6273c3d343a673
Marzhmallow <m.zhylkaidarova@gmail.com>
2026-01-26 19:39:24 +0000

    dockerize consumer service

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -11,12 +11,6 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 if SECRET_KEY is None:
     print("WARNING: SECRET_KEY is NULL")
 
-DATABASE_URL=os.getenv("DATABASE_URL", "postgresql+psycopg2://app:app@postgres:5432/appdb")
+DATABASE_URL=os.getenv("DATABASE_URL")
 
-KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:29092")
-
-S3_ENDPOINT = os.getenv("S3_ENDPOINT")
-S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
-S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
-S3_BUCKER = os.getenv("S3_BUCKET", "event-bucket")
-S3_REGION = os.getenv("S3_REGION", "us-east-1")
+KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
